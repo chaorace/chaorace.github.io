778cbdc18fa4932550e3d6e65021687075648fa9
chaorace <chaorace@gmail.com>
2015-05-07 00:50:56 -0400

    Added a fun dynamic header

---
+++ intros.js
@@ -0,0 +1,37 @@
+function main(){
+  var intros = [
+    "the one and only",
+    "fast and perturbed",
+    "an unhelpful stacktrace",
+    "downloading dependencies",
+    "compiling...",
+    "randomly generated",
+    "decompiling...",
+    "faking it and making it",
+    "in the third person",
+    "losing sleep",
+    "readonly public static object with public sealed class trait extends with SEND_HELP_PLEASE",
+    "inbred",
+    "interactive",
+    "pretending to work",
+    "powered by IE6",
+    "not-such-a-BOFH",
+    "love",
+    "[CLICK ME!]",
+    "chaorace is chaorace is chaorace is chaorace is chaorace is chaorace is",
+    "headbanging",
+    "life",
+    "caffiene-free",
+    "rich in protien",
+    "bad at speling",
+    "secretly a mermaid",
+    "living in denial"
+  ];
+  document.getElementById("introField").innerHTML = intros[getRandomInt(0, intros.length)]
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+main();
