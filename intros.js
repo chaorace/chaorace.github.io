5bf4765bfcb30262bb4e52fc4378f7625d271756
Chris <chaorace@gmail.com>
2015-05-08 14:48:03 -0400

    Update intros.js

--- intros.js
+++ intros.js
@@ -46,7 +46,7 @@ var intros = [
     "spinning up...",
     "updating...",
     "imploding",
-    "tunnel snake(they rule)"
+    "a tunnel snake (they rule)"
   ];
 
 var postLoad = setTimeout(main, 100)
