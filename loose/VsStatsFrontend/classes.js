cbbd3674ff78d77b4c5425a053e18b4c041aad6e
chaorace <chaorace@gmail.com>
2016-06-16 04:52:44 -0400

    Demo updated

---
+++ loose/VsStatsFrontend/classes.js
@@ -0,0 +1,47 @@
+var classes = {
+    DRUID: {
+        value: 0,
+        name: "Druid",
+        color: "#663300"
+    },
+    HUNTER: {
+        value: 1,
+        name: "Hunter",
+        color: "#00CC66"
+    },
+    MAGE: {
+        value: 2,
+        name: "Mage",
+        color: "#009999"
+    },
+    PALADIN: {
+        value: 3,
+        name: "Paladin",
+        color: "#999900"
+    },
+    PRIEST: {
+        value: 4,
+        name: "Priest",
+        color: "#CCCC99"
+    },
+    ROGUE: {
+        value: 5,
+        name: "Rogue",
+        color: "#333333"
+    },
+    SHAMAN: {
+        value: 6,
+        name: "Shaman",
+        color: "#330099"
+    },
+    WARLOCK: {
+        value: 7,
+        name: "Warlock",
+        color: "#663399"
+    },
+    WARRIOR: {
+        value: 8,
+        name: "Warrior",
+        color: "#990000"
+    }
+};
